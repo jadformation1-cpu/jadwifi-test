78abcfcb83a6e5762680b9a202cee6fb758d35ad
jadformation1-cpu <jadformation1@gmail.com>
2025-08-17 10:28:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
 require 'routeros_api.class.php';
 
-$ip       = "hgr0afnb6jp.sn.mynetname.net";  // ton cloud mikrotik
-$user     = "apiuser";                       // l'utilisateur API que tu as créé
-$password = "motdepasse!solide";             // son mot de passe
-$profile  = "1Jour";                         // profil hotspot présent dans Mikrotik
+// ==> Infos de ton Mikrotik
+$ip       = "hgr0afnb6jp.sn.mynetname.net"; // Cloud DDNS Mikrotik
+$user     = "apiuser";                     // utilisateur API (créé sur MikroTik)
+$password = "motdepasse!solide";           // mot de passe
+$profile  = "1Jour";                       // nom du profil hotspot à utiliser
 
-$amount   = 100; // montant fictif juste pour test local
+$amount   = 100; // montant fictif juste pour test
 
-$code     = substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), 0, 6);
+// Génération d’un code aléatoire (login = password)
+$code = substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), 0, 6);
 
+// Connexion via API Mikrotik
 $api = new RouterosAPI();
 if (!$api->connect($ip, $user, $password)) {
     die("ÉCHEC de connexion au MikroTik");
 }
 
+// Création utilisateur Hotspot
 $api->write('/ip/hotspot/user/add', false);
 $api->write('=name='.$code, false);
 $api->write('=password='.$code, false);
@@ -23,4 +27,5 @@ $api->write('=limit-uptime=1d', false);
 $api->write('=comment=testDepuisRender', true);
 $api->read();
 
-echo "Utilisateur créé: ".$code;
+// Affichage résultat
+echo "Utilisateur hotspot créé: ".$code;
